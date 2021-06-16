7ffc79255c476f253aebd0d17cfbed977272d54c
Loan Tytgat <52997111+orionewton@users.noreply.github.com>
2021-06-16 09:51:04 +0200

    Create decsent.py

---
+++ Easy/decsent.py
@@ -0,0 +1,21 @@
+import sys
+import math
+
+# The while loop represents the game.
+# Each iteration represents a turn of the game
+# where you are given inputs (the heights of the mountains)
+# and where you have to print an output (the index of the mountain to fire on)
+# The inputs you are given are automatically updated according to your last actions.
+
+
+# game loop
+while True:
+    max = 0
+    imax = 0
+    for i in range(8):
+        mountain_h = int(input()) # represents the height of one mountain, from 9 to 0. Mountain heights are provided from left to right.
+        if mountain_h > max:
+            max = mountain_h
+            imax = i
+
+    print(imax)
