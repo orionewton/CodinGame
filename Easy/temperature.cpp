16ee837bc5862341cd8c2b7fbd9fe67b4ace3318
Loan Tytgat <52997111+orionewton@users.noreply.github.com>
2021-06-24 13:29:03 +0200

    Create temperature.cpp

---
+++ Easy/temperature.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main(){
+    int n; // the number of temperatures to analyse
+    cin >> n; cin.ignore();
+    int result = 0;
+    if(n > 0){
+        result = 5526;
+        for (int i = 0; i < n; i++) {
+            bool minus = false;
+            int t; // a temperature expressed as an integer ranging from -273 to 5526
+            cin >> t; cin.ignore();
+            if(t < 0){
+                t = abs(t);
+                minus = true;
+            }
+            if(t == abs(int(result))){
+                if(int(result) < 0 && minus){
+                    result = t * -1;
+                }
+                else{
+                    result = t;
+                }
+            }
+            else{
+                if(t < abs(int(result))){
+                    if(minus){
+                        result = t * -1;
+                    }
+                    else{
+                        result = t;
+                    }
+                }
+            }
+        }
+    }
+
+    // Write an answer using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << result << endl;
+}
