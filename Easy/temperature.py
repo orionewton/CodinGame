6103bbc825641c01ff28a9a5dd05f314b959497e
Loan Tytgat <52997111+orionewton@users.noreply.github.com>
2021-06-24 13:32:27 +0200

    Update temperature.py

--- Easy/temperature.py
+++ Easy/temperature.py
@@ -5,9 +5,8 @@ import math
 # the standard input according to the problem statement.
 
 n = int(input())  # the number of temperatures to analyse
-if n == 0:
-    print("0")
-else:
+result = 0
+if n > 0:
     result = 5526
     for i in input().split():
         minus = False
@@ -16,17 +15,17 @@ else:
         if t < 0:
             t = abs(t)
             minus = True
-        if t == abs(int(result)):
-            if int(result) < 0 and minus:
-                result = "-"+str(t)
+        if t == abs(result):
+            if result < 0 and minus:
+                result = t * -1
             else:
                 result = t
-        elif t < abs(int(result)):
+        elif t < abs(result):
             if minus:
-                result = "-"+str(t)
+                result = t * -1
             else:
                 result = t
 
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
-    print(result)
+print(result)
