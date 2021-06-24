0a1b2c27d6d5b09a524fb6978bd13c82f09491f1
Loan Tytgat <52997111+orionewton@users.noreply.github.com>
2021-06-24 11:58:44 +0200

    Create horse.c

---
+++ Easy/horse.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+ 
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+ int compare (const void * a, const void * b)
+{
+  return ( *(int*)a - *(int*)b );
+}
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    int tab[N];
+    int mini = N;
+    for (int i = 0; i < N; i++) {
+        int pi;
+        scanf("%d", &pi);
+        tab[i] = pi;
+    }
+    qsort (tab, N, sizeof(int), compare);
+
+    for(int i = 0; i < N; i++){
+        if(i > 0){
+            mini = MIN(mini, tab[i] - tab[i-1]);
+        }
+    }
+
+    // Write an answer using printf(). DON'T FORGET THE TRAILING \n
+    // To debug: fprintf(stderr, "Debug messages...\n");
+
+    printf("%d\n", mini);
+
+    return 0;
+}
