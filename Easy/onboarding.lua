aad0042e49219239a3cc08fdf6e12f9c43013e0a
Loan Tytgat <52997111+orionewton@users.noreply.github.com>
2021-07-01 10:41:28 +0200

    Create onboarding.lua

---
+++ Easy/onboarding.lua
@@ -0,0 +1,18 @@
+-- CodinGame planet is being attacked by slimy insectoid aliens.
+-- <---
+-- Hint:To protect the planet, you can implement the pseudo-code provided in the statement, below the player.
+
+
+-- game loop
+while true do
+    enemy1 = io.read() -- name of enemy 1
+    dist1 = tonumber(io.read()) -- distance to enemy 1
+    enemy2 = io.read() -- name of enemy 2
+    dist2 = tonumber(io.read()) -- distance to enemy 2
+    
+    if dist1 < dist2 then
+        print(enemy1)
+    else
+        print(enemy2)        
+    end
+end
