61596c26752e79a29f662dab93bcac510193dad5
Loan Tytgat <52997111+orionewton@users.noreply.github.com>
2021-07-01 10:31:54 +0200

    Create onboarding.js

---
+++ Easy/onboarding.js
@@ -0,0 +1,21 @@
+/**
+ * CodinGame planet is being attacked by slimy insectoid aliens.
+ * <---
+ * Hint:To protect the planet, you can implement the pseudo-code provided in the statement, below the player.
+ **/
+
+
+// game loop
+while (true) {
+    const enemy1 = readline(); // name of enemy 1
+    const dist1 = parseInt(readline()); // distance to enemy 1
+    const enemy2 = readline(); // name of enemy 2
+    const dist2 = parseInt(readline()); // distance to enemy 2
+
+    if(dist1 < dist2){
+        console.log(enemy1);
+    }
+    else{
+        console.log(enemy2);
+    }
+}
